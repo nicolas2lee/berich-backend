9a0dd19e3562be257ea20d9c9e53cdbe024411cf
nicolas2lee <nicolas2lee@gmail.com>
2020-08-26 19:41:09 +0200

    [auth] update

--- identity-manager/exposition/src/main/kotlin/tao/identitymanager/exposition/config/SecurityConfig.kt
+++ identity-manager/exposition/src/main/kotlin/tao/identitymanager/exposition/config/SecurityConfig.kt
@@ -3,18 +3,17 @@ package tao.identitymanager.exposition.config
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.core.Authentication
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.security.web.server.WebFilterExchange
 import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository
 import reactor.core.publisher.Mono
 import tao.identitymanager.exposition.jwt.JwtTokenProvider
+import java.net.URI
 
 
 @Configuration
@@ -26,22 +25,13 @@ class SecurityConfig {
         return http
                 .csrf().disable()
                 .authorizeExchange()
-                .pathMatchers("/", "/home", "/user").permitAll()
+                .pathMatchers("/", "/home").permitAll()
                 .anyExchange().authenticated()
-                .and().formLogin().authenticationSuccessHandler(authSuccessHandler(jwtTokenProvider))
+                .and().formLogin().authenticationSuccessHandler(
+                        authSuccessHandler(jwtTokenProvider))
                 .and().logout()
                 .and().securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
                 .build();
-    }
-
-    //@Bean
-    fun userDetailsService(): MapReactiveUserDetailsService {
-        val user: UserDetails = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("user")
-                .roles("USER")
-                .build()
-        return MapReactiveUserDetailsService(user)
     }
 
     @Bean
@@ -59,8 +49,10 @@ class SecurityConfig {
             val response = webFilterExchange.exchange.response
             response.cookies.remove("SESSION")
             val jwt = jwtTokenProvider.createToken("aud", authentication.principal.toString(), 10000)
-            LOG.info("Generated jwt is: [$jwt]")
+            LOG.debug("Generated jwt is: [$jwt]")
             response.headers.add("Set-Cookie", "${AUTHORIZATION_HEADER}=Bearer ${jwt}; HttpOnly")
+            response.statusCode = HttpStatus.FOUND
+            response.headers.location = URI.create("/")
             return Mono.empty<Void>().then()
         }
     }
--- identity-manager/exposition/src/main/kotlin/tao/identitymanager/exposition/controller/UserController.kt
+++ identity-manager/exposition/src/main/kotlin/tao/identitymanager/exposition/controller/UserController.kt
@@ -1,7 +1,6 @@
 package tao.identitymanager.exposition.controller
 
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService
-import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
@@ -10,18 +9,12 @@ import reactor.core.publisher.Mono
 class UserController (val reactiveUserDetailsService: ReactiveUserDetailsService) {
 
     @GetMapping("/welcome")
-    fun test(): Mono<String> {
+    fun welcome(): Mono<String> {
         return Mono.justOrEmpty("welcome")
     }
+
     @GetMapping("/")
-    fun testMain(): Mono<String> {
-        return Mono.justOrEmpty("main")
-    }
-    @GetMapping("/user")
-    fun getUser(): Mono<UserDetails> {
-        println("test")
-        return reactiveUserDetailsService.findByUsername("admin")
-                .checkpoint("a test")
-                .log()
+    fun mainPage(): Mono<String> {
+        return Mono.justOrEmpty("ok")
     }
 }
